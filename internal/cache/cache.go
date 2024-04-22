c92f6dd200237ea0b9d7773e8d6ddaf46055c7d6
Elias <ilyasseyasser3@gmail.com>
2024-04-22 16:53:36 +0100

    added a Getter and a Setter to the cache

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -15,3 +15,19 @@ type Cache struct {
 	cache 	map[string]cacheEntry
 }
 
+func newCacheEntry(val []byte) cacheEntry {
+	return cacheEntry{time.Now(), val}
+}
+
+func (c Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.cache[key] = newCacheEntry(val)
+}
+
+func (c Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	val, exists := c.cache[key]
+	return val.val, exists
+}
