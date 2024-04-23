fafc190dc0852013b54eec8bbd31494657e2ae91
Elias <ilyasseyasser3@gmail.com>
2024-04-23 11:04:43 +0100

    added the reapLoop method

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -31,3 +31,25 @@ func (c Cache) Get(key string) ([]byte, bool) {
 	val, exists := c.cache[key]
 	return val.val, exists
 }
+
+func (c Cache) reapLoop(interval time.Duration) {
+	timeNow := time.Now()
+	for k, v := range(c.cache) {
+		if timeNow.Sub(v.createdAt) > interval {
+			delete(c.cache, k)
+		}
+	}
+}
+
+// func main() {
+// 	cache := Cache{cache : make(map[string]cacheEntry)}
+
+// 	val :=  []byte{1,2,3}
+// 	cache.Add("hola", val)
+
+// 	val, ok := cache.Get("hola")
+// 	fmt.Println(val, ok)
+
+// 	val, ok = cache.Get("holsa")
+// 	fmt.Println(val, ok)
+// }
